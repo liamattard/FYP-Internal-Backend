1cb6f1c470ad12ac7e6257fffb667da9efb26e92
liamattard <liam.dratta@gmail.com>
2021-05-10 13:11:55 +0200

    initial commit with classification model

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from classification_model import scene_detection
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+
+@app.route("/classify_image")
+def main():
+    url = request.args.get("url")
+    decoded_url = unquote(url)
+    print("Recieved URL: ", decoded_url)
+    print("\n...")
+    scene = scene_detection(decoded_url)
+    return scene
+
+
+if __name__ == "__main__":
+    app.run()
